de059ae3736032528675e9df591d5947bbf91fb3
Jasmin <ssongnari@gmail.com>
2022-06-21 14:36:37 -0400

    commit tree a435702f5be0c18804cb6c2fee7cab3dbca24fe0

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,32 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
+import {
+  trees as parkTrees,
+  wildlife as parkWildlife,
+} from "./parks/RockyMountain";
+import * as RMFunctions from "./parks/RockyMountain";
+
+
+console.log(trees);
+
+wildlife();
+
+parkWildlife();
+
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
+}
 
-  return <h1>Colorado State Parks!</h1>;
+export default function ColoradoStateParks() {
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
